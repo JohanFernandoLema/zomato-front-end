3d33c75757f73a501b8c553301fb843569dd10ab
JohanFernandoLema <johan.13fernando@gmail.com>
2023-07-24 10:18:42 -0400

    search component added to main file - loaded in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './components/Home'
+import Search from './components/Search'
 
 function App() {
   return (
     <>
       <main className="container-fluid">
         <Home />
+        <Search />
       </main>
     </>
   )
